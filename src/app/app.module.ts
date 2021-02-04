c678f116dce21e2613f62d9ba4cb20e05187c9fd
Adil <adil.kenesbek21@gmail.com>
2021-02-04 11:47:03 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddRecordComponent } from './add-record/add-record.component';
+import { ResultsComponent } from './results/results.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRecordComponent,
+    ResultsComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
