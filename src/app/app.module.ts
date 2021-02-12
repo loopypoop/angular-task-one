c43a67faffdd4207d33b9bdd25f07c74ba1e042e
Adil <adil.kenesbek21@gmail.com>
2021-02-12 23:50:00 +0600

    footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import {LayoutModule} from './layout/layout.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
